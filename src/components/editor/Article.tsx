97ff1d59867eb723bc1187ee4f1484a8fb5c01bd
Vinod Kotagiri <vinod.kotagiri@solix.com>
2025-01-30 17:34:00 +0530

    Added view edit in article

--- src/components/editor/Article.tsx
+++ src/components/editor/Article.tsx
@@ -3,15 +3,24 @@ import {  useState } from "react"
 import { useAppSelector } from "../../redux/hooks"
 import ArticleViewer from "./ArticleViewer";
 import ArticleEditor from "./ArticleEditor";
+import { FaEdit, FaEye } from "react-icons/fa";
 
 
 function Article() {
-  const [editing,setEditing]=useState(true);
+  const [editing,setEditing]=useState(false);
   const {articleData}=useAppSelector(state=>state.article)
 
   return (
-    <div className="w-[90%] items-center justify-center p-2 h-full bg-slate-700 flex gap-2" style={{backgroundColor:'ghostwhite'}}>
-      <div className="w-full h-full bg-black rounded-md">
+    <div className="w-[90%] items-center justify-center p-2 relative h-full bg-slate-700 flex gap-2 bg-slate-800" >
+      <div className="w-full h-full rounded-md flex flex-col gap-3 bg-slate-800">
+      <div className="h-10 w-full  border-[1px] border-primary rounded-md bg-purple-600 text-slate-200 flex items-center">
+       <div className="tooltip tooltip-right  " data-tip={editing?"view":"edit"}>
+        <button 
+         className="btn btn-ghost hover:bg-transparent outline-none border-none shadow-none"
+         onClick={()=>setEditing(!editing)}
+         >{editing?<FaEye size={24}/>:<FaEdit size={24}/>}</button>
+         </div>
+      </div>
       {!editing?<ArticleViewer articleData={articleData}/>:<ArticleEditor articleData={articleData} />}
       </div>
     </div>
--- src/components/editor/ArticleEditor.tsx
+++ src/components/editor/ArticleEditor.tsx
@@ -90,7 +90,7 @@ const ArticleEditor = ({ articleData, onSave }) => {
   };
 
   return (
-    <div className="w-full h-full bg-white text-slate-900 text-xl rounded-md overflow-y-auto">
+    <div className=" w-full h-full bg-white text-slate-900 text-xl rounded-md overflow-y-auto">
       <ReactQuill theme="snow" value={quillValue} onChange={handleChange} formats={formats} modules={modules}/>
       <button onClick={handleSave} className="mt-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
         Save
