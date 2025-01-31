7594ae8c27a40a24caeed208cde07f9dcfa53e38
Vinod Kotagiri <vinod.kotagiri@solix.com>
2025-01-31 17:00:54 +0530

    changed colors

--- src/components/editor/Article.tsx
+++ src/components/editor/Article.tsx
@@ -11,9 +11,9 @@ function Article() {
   const {articleData}=useAppSelector(state=>state.article)
 
   return (
-    <div className="w-full items-center justify-center  p-2 relative h-full  flex gap-2 relative overflow-hidden" >
+    <div className="w-full items-center bg-gradient-to-r from-pink-600 to-purple-500 justify-center  p-2 relative h-full  flex gap-2 relative overflow-hidden" >
       <div className="w-[70%] h-full rounded-md flex flex-col gap-3 overflow-auto mt-24 ">
-      <div className="absolute h-12 w-12 top-[1%] left-[10%] border-[1px] border-primary rounded-md bg-gradient-to-r from-pink-600 to-purple-500 text-slate-200 flex items-center justify-center">
+      <div className="bg-black absolute h-12 w-12 top-[1%] left-[10%] border-[1px] border-primary rounded-md  text-slate-200 flex items-center justify-center">
        <div className="tooltip tooltip-left  " data-tip={editing?"view":"edit"}>
         <button 
          className="btn btn-ghost hover:bg-transparent outline-none border-none shadow-none"
