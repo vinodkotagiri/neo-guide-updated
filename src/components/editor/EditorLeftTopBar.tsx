97a57692711479cf35ecbf9a65dc6fd6a8c7d370
Vinod Kotagiri <vinodkotagiri@icloud.com>
2025-02-01 08:30:30 +0530

    changed colors

--- src/components/editor/EditorLeftTopBar.tsx
+++ src/components/editor/EditorLeftTopBar.tsx
@@ -19,7 +19,7 @@ function EditorLeftTopBar() {
     <div className='w-full h-[72px] border-slate-700 border-b-[1px] flex items-center px-4 p-2 gap-2'>
       <div className='flex h-full items-center bg-slate-900 rounded-md px-2'>
       <BsTranslate size={32} color='#9810FA'/>
-      <select className="select select-sm outline-none cursor-pointer shadow-none border-none bg-slate-900  focus:ring-0 focus:outline-none" defaultValue={1} onChange={(e)=>setOperation(parseInt(e.target.value) )}>
+      <select className="text-white select select-sm outline-none cursor-pointer shadow-none border-none bg-slate-900  focus:ring-0 focus:outline-none" defaultValue={1} onChange={(e)=>setOperation(parseInt(e.target.value) )}>
         <option value={1}>Translate & Dub</option>
         <option value={2}>Sub-titles</option>
       </select>
@@ -27,7 +27,7 @@ function EditorLeftTopBar() {
 
       <div className='flex h-full items-center bg-slate-900 rounded-md px-2'>
       <GrLanguage size={32 } color='#9810FA'/>
-      <select className="select select-sm outline-none cursor-pointer shadow-none border-none bg-slate-900  focus:ring-0 focus:outline-none" defaultValue={0} onChange={(e)=>setLang(e.target.value)}>
+      <select className="text-white select select-sm outline-none cursor-pointer shadow-none border-none bg-slate-900  focus:ring-0 focus:outline-none" defaultValue={0} onChange={(e)=>setLang(e.target.value)}>
       <option value={null} disabled>Language</option>
         {availableLanguages.map((lang,index)=><option key={index} value={lang.value}>{lang.key}</option>)}
       </select>
@@ -35,7 +35,7 @@ function EditorLeftTopBar() {
 
       {operation==1&&<div className='flex h-full items-center bg-slate-900 rounded-md px-2'>
       <MdSettingsVoice size={32 } color='#9810FA'/>
-      <select className="select select-sm outline-none cursor-pointer shadow-none border-none bg-slate-900  focus:ring-0 focus:outline-none" defaultValue={0} onChange={(e)=>setVoice(e.target.value)}>
+      <select className="text-white select select-sm outline-none cursor-pointer shadow-none border-none bg-slate-900  focus:ring-0 focus:outline-none" defaultValue={0} onChange={(e)=>setVoice(e.target.value)}>
       <option value={null} disabled>Voice</option>
         <option value={'Nitesh'}>Nitesh</option>
       </select>
