bf507a77fbf874730644fcc687ba9cfb3ac91fa6
Vinod Kotagiri <vinod.kotagiri@solix.com>
2025-02-07 17:44:44 +0530

    added buttons

--- src/components/editor/ElementsAddComponent.tsx
+++ src/components/editor/ElementsAddComponent.tsx
@@ -1,22 +1,29 @@
 import React from 'react'
-
+import { MdBlurOn, MdOutlineRectangle, MdOutlineTextFields,MdArrowOutward,MdOutlineCenterFocusStrong } from 'react-icons/md'
+import {GoRepoPush} from 'react-icons/go'
 function ElementsAddComponent() {
   return (
     <div className='w-full flex flex-col'>
-       <div className='w-full border-b-[1px] h-10 border-slate-600 flex items-center justify-between px-2'>
+      <div className='w-full border-b-[1px] h-10 border-slate-600 flex items-center justify-between px-2'>
         Add Elements
-       </div>
-       <div className='flex flex-wrap items-center justify-center p-4 gap-2'>
-
-       </div>
       </div>
+      <div className='flex flex-wrap justify-center items-center py-4 px-1 gap-4'>
+        <ElementTypeComponent icon={<MdBlurOn size={32}/>} label={'blur'}/>
+        <ElementTypeComponent icon={<MdOutlineRectangle size={32}/>} label={'rectangle'}/>
+        <ElementTypeComponent icon={<MdOutlineTextFields size={32}/>} label={'text'}/>
+        <ElementTypeComponent icon={<MdArrowOutward size={32}/>} label={'arrow'}/>
+        <ElementTypeComponent icon={<MdOutlineCenterFocusStrong size={32}/>} label={'spotlight'}/>
+        <ElementTypeComponent icon={<GoRepoPush size={32}/>} label={'pop-over'}/>
+      
+      </div>
+    </div>
   )
 }
 
 export default ElementsAddComponent
 
-function ElementTypeComponent(){
+function ElementTypeComponent({icon,label}) {
   return (
-    <div className='w-20 h-20'></div>
+  <button className='btn btn-accent text-slate-900 w-[160px]'>{icon}<small className='capitalize'>{label}</small></button>
   )
 }
