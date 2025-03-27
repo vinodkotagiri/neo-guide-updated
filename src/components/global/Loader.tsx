34b8766c54ed15af77940df54f7b049243783ea1
Vinod Kotagiri <vinod.kotagiri@solix.com>
2025-03-27 11:08:42 +0530

    fix build error

--- src/components/global/Loader.tsx
+++ src/components/global/Loader.tsx
@@ -8,7 +8,7 @@ const Loader = () => {
 
   return (
     <div className="fixed top-0 left-0 w-screen h-screen flex flex-col justify-center gap-2 items-center bg-black bg-opacity-90 z-[9999]">
-      <ScaleLoader size={64} color="oklch(0.707 0.165 254.624)" />
+      <ScaleLoader  color="oklch(0.707 0.165 254.624)" />
 
       <div className="flex flex-col items-center justify-center">
         {percentage ? <pre className="capitalize text-xs text-blue-400 text-center font-bold font-serif">{percentage}%</pre>:''}
